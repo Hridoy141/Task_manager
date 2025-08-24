91b699817b3ddb6c6e3fddfeb472e5099a735c95
Hridoy Komol Souman <hridoykomolsouman@gmail.com>
2025-08-24 23:23:53 +0600

    live_score_app

---
+++ firebase_demo/lib/main.dart
@@ -0,0 +1,188 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_demo/fcm_service.dart';
+import 'package:flutter/material.dart';
+// Import the generated file
+import 'firebase_options.dart';
+// void main() => runApp(const MyApp()) ;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FCMService().initialize();
+  await FCMService().getFcmToken();
+  await FCMService().onTokenRefresh();
+
+  runApp(const LiveScoreApp());
+}
+
+class LiveScoreApp extends StatelessWidget {
+  const LiveScoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<LiveScore> _listofScore = [];
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  Future<void> _getLiveScoreData() async {
+    _listofScore.clear();
+    final QuerySnapshot<Map<String, dynamic>> snapShots = await db
+        .collection('football')
+        .get();
+    for (QueryDocumentSnapshot<Map<String, dynamic>> doc in snapShots.docs) {
+      LiveScore liveScore = LiveScore(
+        id: doc.id,
+        team1Name: doc.get('team1_name'),
+        team2Name: doc.get('team1_name'),
+        team1Score: doc.get('team1_score'),
+        team2Score: doc.get('team2_score'),
+        isRunning: doc.get('is_running'),
+        winnerTeam: doc.get('winner_team'),
+      );
+      _listofScore.add(liveScore);
+    }
+    setState(() {});
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _getLiveScoreData();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: StreamBuilder(
+        stream: db.collection('football').snapshots(),
+        builder:
+            (
+              context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshots,
+            ) {
+              if (snapshots.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              if (snapshots.hasError) {
+                return Center(child: Text(snapshots.error.toString()));
+              }
+              if (snapshots.hasData) {
+                _listofScore.clear();
+                for (QueryDocumentSnapshot<Map<String, dynamic>> doc
+                    in snapshots.data!.docs) {
+                  LiveScore liveScore = LiveScore(
+                    id: doc.id,
+                    team1Name: doc.get('team1_name'),
+                    team2Name: doc.get('team1_name'),
+                    team1Score: doc.get('team1_score'),
+                    team2Score: doc.get('team2_score'),
+                    isRunning: doc.get('is_running'),
+                    winnerTeam: doc.get('winner_team'),
+                  );
+                  _listofScore.add(liveScore);
+                }
+              }
+
+              return ListView.builder(
+                itemCount: _listofScore.length,
+                itemBuilder: (context, index) {
+                  LiveScore liveScore = _listofScore[index];
+
+                  return ListTile(
+                    onLongPress: () {
+                      db.collection('football').doc(liveScore.id).delete();
+                    },
+                    leading: CircleAvatar(
+                      radius: 10,
+                      backgroundColor: liveScore.isRunning
+                          ? Colors.green
+                          : Colors.grey,
+                    ),
+                    title: Text(liveScore.id),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(liveScore.team1Name),
+                            Text('  vs  '),
+                            Text(liveScore.team2Name),
+                          ],
+                        ),
+                        Text('Is Running: ${liveScore.isRunning}'),
+                        Text('Winner team: ${liveScore.winnerTeam}'),
+                      ],
+                    ),
+                    trailing: Text(
+                      '${liveScore.team1Score} : ${liveScore.team2Score}',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  );
+                },
+              );
+            },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          LiveScore liveScore = LiveScore(
+              id: 'argvsgermany',
+              team1Name: 'Argentina',
+              team2Name: 'Germany',
+              team1Score: 2,
+              team2Score: 4,
+              isRunning: true,
+              winnerTeam: ''
+          );
+
+          await db.collection('football')
+              .doc(liveScore.id)
+              .set(liveScore.toMap());
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class LiveScore {
+  final String id;
+  final String team1Name;
+  final String team2Name;
+  final int team1Score;
+  final int team2Score;
+  final bool isRunning;
+  final String winnerTeam;
+
+  LiveScore({
+    required this.id,
+    required this.team1Name,
+    required this.team2Name,
+    required this.team1Score,
+    required this.team2Score,
+    required this.isRunning,
+    required this.winnerTeam,
+  });
+  Map<String, dynamic> toMap() {
+    return {
+      'team1_name' : team1Name,
+      'team2_name' : team2Name,
+      'team1_score' : team1Score,
+      'team2_score' : team2Score,
+      'is_running' : isRunning,
+      'winner_team' : winnerTeam,
+    };
+  }
+}
