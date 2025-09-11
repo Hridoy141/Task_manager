4ab984a1e5d55fb42a72f99468234864464d32c6
Hridoy Komol Souman <hridoykomolsouman@gmail.com>
2025-09-11 23:07:28 +0600

    Add localization set up

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,9 +1,8 @@
-import 'dart:ui';
 import 'package:e_commerce/app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -12,9 +11,9 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-
   // Pass all uncaught "fatal" errors from the framework to Crashlytics
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
 
   // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
   PlatformDispatcher.instance.onError = (error, stack) {
